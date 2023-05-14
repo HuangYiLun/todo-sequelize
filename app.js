a0cde5ba8005b99e7e026cc9d5bed77b33fe0c88
HuangYiLun <wk5018102@gmail.com>
2023-05-14 20:12:51 +0800

    feat: add facebook login

--- app.js
+++ app.js
@@ -10,11 +10,15 @@ const routes = require('./routes')
 const app = express()
 const port = 3000
 
+if (process.env.NODE_ENV !== "production") {
+  require('dotenv').config()
+}
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(session({
-  secret: 'ThisIsMySecret',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,10 @@ const router = express.Router()
 const users = require('./modules/users')
 const home = require('./modules/home')
 const todos = require('./modules/todos')
+const auth = require('./modules/auth')
 
 router.use('/users', users)
+router.use('/auth', auth)
 router.use('/todos', todos)
 router.use('/', home)
 
