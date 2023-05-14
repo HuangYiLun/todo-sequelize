933de85a64151cbb2d573ede1c41e7c46432223f
HuangYiLun <wk5018102@gmail.com>
2023-05-14 19:06:56 +0800

    feat: add edit todo route

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -19,6 +19,10 @@ router.post('/', (req, res) => {
 })
 
 router.get('/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
 
 })
 
